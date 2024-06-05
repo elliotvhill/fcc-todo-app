67aaa490f2cad54b9a20a9815857e65f62f78d17
Elliot <ellthrowaway2@icloud.com>
2024-06-05 15:30:25 -0400

    building edit and delete task functionality

--- script.js
+++ script.js
@@ -64,23 +64,44 @@ const addOrUpdateTask = () => {
     }
     if (dataArrIndex === -1) {
         taskData.unshift(taskObj)
+    } else {
+        taskData[dataArrIndex] = taskObj
     }
     updateTaskContainer()
     reset()
 }
 
 const updateTaskContainer = () => {
-    tasksContainer.innerHTML = ""
-    taskData.forEach(
-        ({ id, title, date, description }) => {
+    tasksContainer.innerHTML = ''
+    taskData.forEach(({ id, title, date, description }) => {
         tasksContainer.innerHTML += `
             <div class="task" id="${id}">
             <p><strong>Title:</strong> ${title}</p>
             <p><strong>Date:</strong> ${date}</p>
             <p><strong>Description:</strong> ${description}</p>
-            <button type="button" class="btn">Edit</button>
-            <button type="button" class="btn">Delete</button>
+            <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+            <button onclick="deleteTask(this)" type="button" class="btn">Delete</button>
             </div>
             `
     })
+}
+
+const deleteTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    )
+    buttonEl.parentElement.remove()
+    taskData.splice(dataArrIndex, 1)
+}
+
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    )
+    currentTask = taskData[dataArrIndex]
+    titleInput.value = currentTask.title
+    dateInput.value = currentTask.date
+    descriptionInput.value = currentTask.description
+    addOrUpdateTaskBtn.innerText = "Update Task"
+    taskForm.classList.toggle("hidden")
 }
