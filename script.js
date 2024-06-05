425ea130c891d59d5db04743e680c081973a2d67
Elliot <ellthrowaway2@icloud.com>
2024-06-05 14:10:53 -0400

    event listeners to open and close task modal

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ const tasksContainer = document.getElementById("tasks-container")
 const titleInput = document.getElementById("title-input")
 const dateInput = document.getElementById("date-input")
 const descriptionInput = document.getElementById("description-input")
+
+const taskData = []
+let currentTask = {}
+
+openTaskFormBtn.addEventListener("click", () => { taskForm.classList.toggle("hidden") })
+closeTaskFormBtn.addEventListener("click", () => { confirmCloseDialog.showModal() })
+cancelBtn.addEventListener("click", () => { confirmCloseDialog.close() })
+discardBtn.addEventListener("click", () => {
+    confirmCloseDialog.close()
+    taskForm.classList.toggle("hidden")
+})
